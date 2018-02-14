d2dd55b88bc0af5a14370bdced4114d53fb7ba6b
IN\rasal.shukla <rasal.shukla@tavant.com>
2018-02-14 15:57:44 +0530

    Priority Html Modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgRedux , NgReduxModule } from '@angular-redux/store';
 import { IAppState , rootReducer, INITIAL_STATE } from './store';
-
-
 import { AppComponent } from './app.component';
 import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
